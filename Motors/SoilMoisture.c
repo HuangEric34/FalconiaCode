6b3b8744f2e9647d75221889eed69d86455834cf
Karthik Vedula <67874638+oboy-1@users.noreply.github.com>
2023-02-23 10:23:27 -0500

    Update SoilMoisture.c

--- Motors/SoilMoisture.c
+++ Motors/SoilMoisture.c
@@ -4,9 +4,14 @@ from adafruit_motorkit import MotorKit
 
 kit = MotorKit(i2c = board.I2C())
   
-kit.motor3.throttle = 1.0
-
-time.sleep(4)
-
+kit.motor3.throttle = 0.5
+time.sleep(2)
 
 kit.motor3.throttle = 0
+time.sleep(10)
+  
+kit.motor3.throttle = -0.5
+time.sleep(2)
+  
+kit.motor3.throttle = 0
+time.sleep(3)
