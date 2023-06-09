3759f4ab93aac1438618862a7152e2b38edb6639
HuangEric <66891965+HuangEric34@users.noreply.github.com>
2023-06-08 20:53:26 -0400

    Create hbridge_raw.py

---
+++ Motors/Blimp/hbridge_raw.py
@@ -0,0 +1,57 @@
+import RPi.GPIO as gpio
+import time
+
+LM1 = 6
+LM2 = 26
+# RM1 = 
+# RM2 = 
+# CM1 = 
+# CM2 = 
+
+def init():    
+    gpio.setmode(gpio.BCM)
+    gpio.setwarnings(False)
+    gpio.setup(LM1, gpio.OUT)
+    gpio.setup(LM2, gpio.OUT)
+    # gpio.setup(RM1, gpio.OUT)
+    # gpio.setup(RM2, gpio.OUT)
+    # gpio.setup(CM1, gpio.OUT)
+    # gpio.setup(CM2, gpio.OUT)
+
+def forward(sec):
+    gpio.output(LM1, False)
+    gpio.output(LM2, True)
+    # gpio.output(RM1, True)
+    # gpio.output(RM2, False)
+    time.sleep(sec)
+
+# def back(sec):
+    gpio.output(LM1, True)
+    gpio.output(LM2, False)
+    gpio.output(RM1, False)
+    gpio.output(RM2, True)
+    time.sleep(sec)
+
+# def left(sec):
+    gpio.output(LM1, True)
+    gpio.output(LM2, False)
+    gpio.output(RM1, True)
+    gpio.output(RM2, False)
+    time.sleep(sec)
+
+# def right(sec):
+    gpio.output(LM1, False)
+    gpio.output(LM2, True)
+    gpio.output(RM1, False)
+    gpio.output(RM2, True)
+    time.sleep(sec)
+
+# def up(sec):
+    gpio.output(CM1, False)
+    gpio.output(CM2, True)
+
+# def down(sec):
+    gpio.output(CM1, True)
+    gpio.output(CM2, False)
+
+gpio.cleanup()
