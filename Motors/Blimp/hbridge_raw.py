8eb097f152b8240c40ab758197e7bdb032bb13c0
HuangEric <66891965+HuangEric34@users.noreply.github.com>
2023-06-08 20:53:59 -0400

    Update hbridge_raw.py

--- Motors/Blimp/hbridge_raw.py
+++ Motors/Blimp/hbridge_raw.py
@@ -25,32 +25,32 @@ def forward(sec):
     # gpio.output(RM2, False)
     time.sleep(sec)
 
-# def back(sec):
+def back(sec):
     gpio.output(LM1, True)
     gpio.output(LM2, False)
     gpio.output(RM1, False)
     gpio.output(RM2, True)
     time.sleep(sec)
 
-# def left(sec):
+def left(sec):
     gpio.output(LM1, True)
     gpio.output(LM2, False)
     gpio.output(RM1, True)
     gpio.output(RM2, False)
     time.sleep(sec)
 
-# def right(sec):
+def right(sec):
     gpio.output(LM1, False)
     gpio.output(LM2, True)
     gpio.output(RM1, False)
     gpio.output(RM2, True)
     time.sleep(sec)
 
-# def up(sec):
+def up(sec):
     gpio.output(CM1, False)
     gpio.output(CM2, True)
 
-# def down(sec):
+def down(sec):
     gpio.output(CM1, True)
     gpio.output(CM2, False)
 
